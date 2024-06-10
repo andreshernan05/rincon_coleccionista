be0f540efb376b7a212973ce45d075a8eba6e444
andreshernan05 <andres.lucero05@hotmail.com>
2024-06-10 19:27:32 -0300

    master

--- js/index.js
+++ js/index.js
@@ -1,95 +1,31 @@
 const carrito = []
+const contCarrito = document.getElementById("contador__carrito")
+const imgCarrito = document.getElementById("carrito")
+const buscador = document.querySelector("input#buscador")
+const filtroRadio = document.querySelector("input#filtros__radial")
 
 function principal() {
-    alert("TODA LA INFORMACION SERA PROYECTADA EN LA CONSOLA")
-    let opcion = parseInt(prompt("¿Que desea hacer? \n 1- Ver Productos \n 2- Comprar \n 3- Ver formas de pago \n 4- Ver Carrito \n 5- Salir "))
-
-    if (opcion <= 5) {
-        switch (opcion) {
-            case 1:
-                verProductos()
-                principal()
-                break
-
-            case 2:
-                verProductos()
-                comprar()
-                break
-
-            case 3:
-                pago()
-                principal()
-                break
-
-            case 4:
-                compraspendientes()
-                break
-
-            case 5:
-                salir()
-                break
-
-        } // LLAVE DE CIERRE SWITCH
-    } // LLAVE DE CIERRE IF
+    carga__articulos(autitos)
+    enableBtn()
 }
-// OPCION 1 VER PRODUCTOS
-function verProductos() {
-    console.table(autitos)
-}// OPCION 1 VER PRODUCTOS
 
-//OPCION 2 COMPRAR
-function buscarproducto(codigo) {
-    let artelegico = autitos.find((auto) => auto.codart === codigo)
-    return artelegico
+function contadorCarrito() {
+    contCarrito.textContent = carrito.length
 }
-function comprar() {
-    let codigo = parseInt(prompt("Ingrese el codigo del producto \n ingrese cero para finalizar la compra"))
-    if (codigo != 0) {
-        let prodelegido = buscarproducto(codigo)
-        if (prodelegido === undefined) {
-            alert("No se encontro el producto seleccionado")
-            comprar()
-        } else {
-            carrito.push(prodelegido)
-            let rta = confirm("¿Desea comprar otro producto?")
-            if (rta === true) {
-                comprar()
-            } else {
-                principal()
-            }
-        }
-    } else {
-        principal()
-    }
-}//OPCION 2 COMPRAR
 
-//OPCION 3 VER FORMA DE PAGOS
-function pago() {
-    console.table(pagos)
-}//OPCION 3 VER FORMA DE PAGOS
+function enableBtn() {
+    const bntadd = document.querySelectorAll("button.boton")
 
-//OPCION 4 VER CARRITO
-function subtotal() {
-    if (carrito.length > 0) {
-        return carrito.reduce((acc, autito) => acc + autito.precio, 0)
+    if (bntadd.length > 0) {
+        bntadd.forEach((boton) => {
+            boton.addEventListener("click", () => {
+                const prodselect = autitos.find((autito) => autito.codart == boton.codart)
+                carrito.push(prodselect)
+                contadorCarrito()
+            })
+        })
     }
 }
-function compraspendientes() {
-    console.table(carrito)
-    console.log(" El total a pagar es de: $" + subtotal())
-
-}//OPCION 4 VER CARRITO
-
-//OPCION 5 SALIR
-function salir() {
-    let rta = confirm("¿Desea Salir?")
-    if (rta === true) {
-        alert("Gracias por su visita, vuelva pronto")
-    } else {
-        principal()
-    }
-}//OPCION 5 SALIR
-
 // CREACION DE CARDS
 
 function card__html(autito) {
@@ -98,18 +34,37 @@ function card__html(autito) {
                     <img src="${autito.imagen}">
                     <figcaption>
                         <h3>${autito.nombre}</h3>
-                        <h4>${autito.precio}</h4>
+                        <h4>$${autito.precio}</h4>
+                        <div>
+                            <button id:"${autito.codart}" class="add-btn boton">Agregar a Carrito</button>
+                        </div>
                     </figcaption>
+                    
                 </figure>
             </div>`
 }
 
-function carga__articulos() {
+function carga__articulos(autitos) {
     if (autitos.length > 0) {
         container__cards.innerHTML = ""
         autitos.forEach((autito) => container__cards.innerHTML += card__html(autito))
     }
 }
 
-carga__articulos()
+/*filtroRadio.addEventListener("click", (e)=>{
+    switch (filtroRadio.value){
+        case "sth":
+            let dbSth = autitos.filter((autito)=>autito.)
+    }
+})*/
+
+// EVENTOS 
+buscador.addEventListener("keydown", (e) => {
+    let resultado = autitos.filter((autito) => autito.nombre.toLowerCase().includes(buscador.value.toLowerCase()))
+    carga__articulos(resultado)
+})
+
+
+
+// FUNCION PRINCIPAL
 principal()
